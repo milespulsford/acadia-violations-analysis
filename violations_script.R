2690933436ef39107b5e6a74b8b95b3e83cd751a
milespulsford <miles.pulsford@gmail.com>
2018-02-28 10:39:07 -0500

    Update violations_script.R

--- violations_script.R
+++ violations_script.R
@@ -48,33 +48,3 @@ violations_per_inspection <- inspections_data %>%
   mutate(inspection_type = (ownership + was_inspection_acadia)) %>%
   group_by(inspection_type) %>%
   summarize(violations_per_inspection = (n() / n_distinct(EVENT_ID)))
-
-
-forum_theme <- theme(
-  panel.grid.major.y = element_blank(),
-  panel.grid.major.x = element_line(color = '#CDCDCD'),
-  panel.grid.minor = element_blank(),
-  panel.background = element_rect(fill = 'white'),
-  axis.ticks = element_blank(),
-  plot.margin = unit(c(25, 25, 25, 25), 'point'),
-  axis.text = element_text(color = '#333333', size = 10),
-  axis.title = element_text(color = '#333333', size = 12),
-  plot.title = element_text(hjust = 0, margin = margin(0,0,25,0), size = 16),
-  legend.position = "none"
-)
-
-vpichart_data <- violations_per_inspection %>%
-  mutate(highlight = ifelse(inspection_type == 4, 'yes', 'no'))
-
-violations_chart <- ggplot(vpichart_data, aes(x = inspection_type,
-                                              y = violations_per_inspection,
-                                              fill = highlight)) +
-  geom_col(width = 0.7) +
-  scale_fill_manual(values = c('yes' = '#6B0F1A', 'no' = '#0e0e40')) +
-  labs(title = 'Hospital Inspection Violations', x = 'Facility Ownership',
-       y = 'Violations Per Inspection') +
-  ylim(0, 7) +
-  coord_flip() +
-  forum_theme
-
-ggsave('violations_chart.pdf', violations_chart)
